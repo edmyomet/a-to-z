c13d3cea10e53aafbf092d76f447f56fc72d4388
ausec-tech-team <03shruthi.b@gmail.com>
2024-06-26 15:48:19 +0530

    Working gitignore
    Gitignore is now working

---
+++ numpy_documentation/np-doc1.py
@@ -0,0 +1,17 @@
+"""
+        numpy documentation
+    """
+import numpy as np 
+
+#sample 
+m = np.arange(10)
+print(type(m))
+
+class Matrix:
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    pass
